9c41fe10585ef1cebd109bc5ad81bdfe59b02930
henrylaifeng <e0032134@u.nus.edu>
2017-10-19 16:56:38 +0800

    complete 2nd tut of javafx

--- src/seedu/address/MainApp.java
+++ src/seedu/address/MainApp.java
@@ -2,17 +2,55 @@ package seedu.address;
 
 import java.io.IOException;
 
+
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import seedu.address.model.Person;
+import seedu.address.view.PersonOverviewController;
+
 
 public class MainApp extends Application {
     private Stage primaryStage;
     private BorderPane rootLayout;
 
+    // ... AFTER THE OTHER VARIABLES ...
+
+    /**
+     * The data as an observable list of Persons.
+     */
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
+
+    /**
+     * Constructor
+     */
+    public MainApp() {
+        // Add some sample data
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+    }
+
+    /**
+     * Returns the data as an observable list of Persons.
+     * @return
+     */
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
+
+    // ... THE REST OF THE CLASS ...
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
@@ -21,6 +59,8 @@ public class MainApp extends Application {
         initRootLayout();
 
         showPersonOverview();
+
+
     }
 
     /**
@@ -54,6 +94,10 @@ public class MainApp extends Application {
 
             // Set person overview into the center of root layout.
             rootLayout.setCenter(personOverview);
+
+            // Give the controller access to the main app.
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
         } catch (IOException e) {
             e.printStackTrace();
         }
